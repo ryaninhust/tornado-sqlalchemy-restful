97029d36934d661bde5634b79b004042ba7b7e13
yuanbowen <yuanbowen@douban.com>
2013-09-03 21:52:14 +0800

    init the template

---
+++ app.py
@@ -0,0 +1,67 @@
+# Python imports
+
+# Tornado imports
+import tornado.auth
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+from tornado.web import url
+
+# Sqlalchemy imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# App imports
+import models
+
+# Options
+define("port", default=8000, help="run on the given port", type=int)
+define("debug", default=False, type=bool)
+define("db_path", default='sqlite:////tmp/test.db', type=str)
+
+
+class Application(tornado.web.Application):
+
+    def __init__(self):
+        handlers = [
+            url(r'/', IndexHandler, name='index'),
+        ]
+        settings = dict(
+            debug=options.debug,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+        engine = create_engine(
+            options.db_path, convert_unicode=True, echo=options.debug)
+        models.init_db(engine)
+        self.db = scoped_session(sessionmaker(bind=engine))
+
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    @property
+    def db(self):
+        return self.application.db
+
+
+class IndexHandler(BaseHandler):
+
+    def get(self):
+        self.write({'init': 'hello world'})
+
+    def post(self):
+        self.write({'init': 'hello world'})
+
+
+# Write your handlers here
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
